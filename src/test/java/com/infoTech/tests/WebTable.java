5ed6a5a726efd25dc603f6753f72fdc7a1b275af
ihsankurt17 <ihsankurt@marun.edu.tr>
2022-08-04 13:12:07 +0200

    try2

--- src/test/java/com/infoTech/tests/WebTable.java
+++ src/test/java/com/infoTech/tests/WebTable.java
@@ -1,7 +1,30 @@
 package com.infoTech.tests;
 
 import com.infoTech.utilities.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
 
 public class WebTable extends TestBase {
 
+
+//    @Test  //=> bunun yerine daha kullanisli bir method yazacagiz.
+//    public void table(){
+//        driver.get("https://demo.guru99.com/test/web-table-element.php");
+//    }
+@Test
+    public void girisSayfasi() throws InterruptedException {
+        driver.get("https://demo.guru99.com/test/web-table-element.php");
+        WebElement guvenDuvari = driver.findElement(By.xpath("(//*[.='Accept All'])[4]"));
+        Thread.sleep(6000);
+         guvenDuvari.click();
+    }
+
+//    @Test
+//    public void table() throws InterruptedException {
+//        girisSayfasi();
+//        Thread.sleep(4000);
+//
+//    }
 }
--- src/test/java/com/infoTech/utilities/TestBase.java
+++ src/test/java/com/infoTech/utilities/TestBase.java
@@ -24,7 +24,7 @@ public abstract class TestBase {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
     }
 
